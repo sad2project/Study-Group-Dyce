959cd8b00ea3a5dded4e0b611591befa31b0ba9a
jzimmerman <jzimmerman@collaborative.com>
2016-03-10 13:01:37 -0600

    More Die __init__ plus roll()
    __init__ checks for valid lists of faces
    roll returns a blah Roll
    Roll class created

--- dyce/die.py
+++ dyce/die.py
@@ -2,7 +2,21 @@
 class Die:
     def __init__(self, name, faces):
         if name is None or len(name.strip()) == 0:
-            raise TypeError("Name must not be empty")
+            raise TypeError("Name must not be empty.")
+
+        if faces is None or len(faces) < 2:
+            raise TypeError("Faces must be at least two faces.")
+
+        if None in faces:
+            raise TypeError("No faces can be None.")
 
         self.name = name
         self.faces = faces
+
+    def roll(self):
+        return Roll(self)
+
+
+class Roll:
+    def __init__(self, die):
+        self.die = die
--- tests/dyce/test_die.py
+++ tests/dyce/test_die.py
@@ -3,19 +3,55 @@ from dyce.die import Die
 
 
 class Die_Test (TestCase):
+    def setUp(self):
+        self.someName = "Test"
+        self.someFaces = [0, 0]
+
     def test_can_create_with_name_and_faces(self):
-        die = Die("a name", [])
+        die = Die(self.someName, self.someFaces)
 
         self.assertIsNotNone(die)
 
     def test_name_cannot_be_empty(self):
         with self.assertRaises(TypeError):
-            die = Die("", [])
+            die = Die("", self.someFaces)
 
     def test_name_cannot_be_whitespace(self):
         with self.assertRaises(TypeError):
-            die = Die(" \t", [])
+            die = Die(" \t", self.someFaces)
 
     def test_name_cannot_be_None(self):
         with self.assertRaises(TypeError):
-            die = Die(None, [])
+            die = Die(None, self.someFaces)
+
+    def test_faces_cannot_be_None(self):
+        with self.assertRaises(TypeError):
+            die = Die(self.someName, None)
+
+    def test_faces_cannot_be_empty(self):
+        with self.assertRaises(TypeError):
+            die = Die(self.someName, [])
+
+    def test_faces_cannot_have_one_face(self):
+        with self.assertRaises(TypeError):
+            die = Die(self.someName, [1])
+
+    def test_faces_cannot_contain_None(self):
+        with self.assertRaises(TypeError):
+            self.someFaces.append(None)
+            die = Die(self.someName, self.someFaces)
+
+    def test_roll_is_not_None(self):
+        die = Die(self.someName, self.someFaces)
+
+        result = die.roll()
+
+        self.assertIsNotNone(result)
+
+    def test_roll_returns_a_Roll_with_die(self):
+        die = Die(self.someName, self.someFaces)
+
+        result = die.roll()
+
+        self.assertEqual(result.die, die)
+
